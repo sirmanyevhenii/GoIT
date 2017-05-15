2f5024dfe5d7f3976d6f1752b78afd0dcfa00301
Sirman Yevhenii <sirmanj92@gmail.com>
2017-05-15 14:38:51 +0200

    completed and sent for testing

--- js_1-2/script/script.js
+++ js_1-2/script/script.js
@@ -1,18 +1,15 @@
-// function pow(number, power) {
+// var number, power;
+// number = prompt('Enter your number');
+// power = prompt('bring to this power');
 
+// function pow(number, power) {
 // 	for (var i = 1; i < power; i++) {
 // 		number *= number;
 // 	}
-
 // 	return number;
 // }
 
-// var number, pow;
-// number = prompt("Enter your number", '');
-// power = prompt("bring to this power", '');
-
 // console.log(pow(number, power));
-
 
 
 // ----------------- 2 task --------------------------
@@ -20,22 +17,20 @@
 
 
 
-var name, enterName, enterYourName;
-  name = [];
-  enterName = prompt('Enter any name');
-
+var arr = [];
 for (var i = 0; i < 5; i++) {
-	name.push('enterName');
+	arr[i] = prompt('Enter ' + (5 - i) + ' names');
 }
 
-  enterYourName = prompt('Enter your name');
+console.log(arr);
 
+var userName = prompt('Enter your name');
 for (var i = 0; i < 5; i++) {
-	if (name === enterYourName) {
-		alert(enterYourName + 'your entrance is successful');
-	} else if (enterYourName !== name) {
+	if (arr[i] == userName) {
+		alert(userName + ', your entrance is successful');
+		break;
+	} else {
 		alert('Access error');
+		break;
 	}
 }
-
-// return;

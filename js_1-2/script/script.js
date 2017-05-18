fcc3765d2b6b74495308a5c28026ca9eab2cf055
Sirman Yevhenii <sirmanj92@gmail.com>
2017-05-18 07:54:00 +0200

    bug fixes 5

--- js_1-2/script/script.js
+++ js_1-2/script/script.js
@@ -1,34 +1,32 @@
-// var number, power;
-// number = window.prompt('Enter your number');
-// power = window.prompt('bring to this power');
+var number, power;
+number = prompt('Enter your number');
+power = prompt('bring to this power');
 
-// function pow(number, power) {
-// 	for (var i = 1; i < power; i++) {
-// 		number *= number;
-// 	}
-// 	return number;
-// }
+function pow(number, power) {
+	for (var i = 1; i < power; i++) {
+		number *= number;
+	}
+	return number;
+}
 
-// console.log(pow(number, power));
-
-
+console.log(pow(number, power));
 
 // ----------------- 2 task --------------------------
 // ---------------------------------------------------
 
-
-
 var arr = [];
 for (var i = 0; i < 5; i++) {
-	arr[i] = window.prompt('Enter ' + (5 - i) + ' names');
+    arr[i] = prompt('Enter ' + (5 - i) + ' names');
 }
 
-var userName = window.prompt('Enter your name');
-for (var i = 0; i < arr.length; i++) {
-	if (arr[i] === userName) {
-		window.alert(userName + ', your entrance is successful');
-	} else {
-		window.alert('Access error');
-		break;
-	}
+var userName = prompt('Enter your name');
+for (var i = 0; i < 5; i++) {
+    if (arr[i] == userName) {
+    alert(userName + ', your entrance is successful');
+    break;
+    }
+}
+
+if (i >= 5) {
+    alert('Access error');
 }
